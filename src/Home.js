cb48d9844011a606ebe46b9a8a5ad0564bdbb006
josephredmond5 <josephredmond5@icloud.com>
2023-08-08 21:10:01 +0100

    added an onClick event to my button and used functions as props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./blogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -6,19 +7,18 @@ const Home = () => {
       { title: 'welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
       { title: 'web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }  
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBlogs)
+    }
     return (
-      <div className="home">
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p>written by { blog.author }</p>
-
-            </div>
-
-        ))}
-      </div>
+    <div className="home">
+        <BlogList blogs={blogs} title="all blogs" handleDelete={handleDelete} />
+    </div>
     );
-  }
+}
    
-  export default Home;
+export default Home;
+
   
